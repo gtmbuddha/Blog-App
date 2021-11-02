c3f92fe898020ecb7453cf9c651cfef4cd6a8486
nimeshGautam <nimesgautam@outlook.com>
2021-11-02 15:07:33 +0545

    Commit after all functionality:1st iteration

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HomeView, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView
+from django.urls.conf import include
+from .views import HomeView, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView, SignupView
 
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
@@ -7,4 +8,8 @@ urlpatterns = [
     path('/post/create', PostCreateView.as_view(), name = 'post_create'),
     path('/post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('/post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('/accounts/', include('django.contrib.auth.urls')),
+    path('/accounts/signup/', SignupView.as_view(), name='signup'),
+    
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import Post
 from django.urls import reverse_lazy
+from .forms import SignUpForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class HomeView(ListView):
     model = Post
@@ -10,18 +12,31 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
-    fields = ['title','author','body']
+    fields = ['title','body']
 
-class PostDeleteView(DeleteView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin ,DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
     
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin ,UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class SignupView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+    
+
+
+
     
